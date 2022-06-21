e4e0519d68eddc962def2166ca73575ff5dbdbdc
Tyler <t.angley@protonmail.com>
2022-06-20 23:18:45 -0400

    update configs for building

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,83 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    mode: 'development',
-    context: path.join(__dirname, 'app'),
-    entry: ['./js/app.ts'],
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'app/index.html'),
-            inject: 'body'
-        })
-    ]
+module.exports = (env) => {
+		return {
+		mode: 'development',
+		context: path.join(__dirname, 'app'),
+		entry: {
+			app: './js/app.ts',
+			styles: './css/main.pcss'
+		},
+		output: {
+			filename: '[name].js',
+			path: path.join(__dirname, 'public'),
+			chunkFilename: '[id].[chunkhash].js'
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: path.resolve(__dirname, 'app/index.html'),
+				inject: 'body'
+			}),
+			new MiniCssExtractPlugin()
+		],
+		module: {
+			rules: [
+				{
+					test: /\.ts$/,
+					use: 'ts-loader',
+					exclude: /node_modules/
+				},
+				{
+				   test: /\.p?(css)$/i,
+				   oneOf: [
+					   {
+						   resourceQuery: /module/,
+						   use: [
+							   MiniCssExtractPlugin.loader,
+							   {
+								   loader: 'css-loader',
+								   options: {
+									   url: false,
+									   sourceMap: env.production !== true,
+									   importLoaders: 1,
+									   modules: {
+										   localIdentName: '[local]_[hash:base64:5]'
+									   }
+								   }
+							   },
+							   {
+								   loader: 'postcss-loader',
+								   options: {
+									   sourceMap: env.production !== true
+								   }
+							   }
+						   ]
+					   },
+					   {
+						   use: [
+							   MiniCssExtractPlugin.loader,
+							   {
+								   loader: 'css-loader',
+								   options: {
+									   url: false,
+									   sourceMap: env.production !== true,
+									   importLoaders: 1,
+									   modules: false
+								   }
+							   },
+							   {
+								   loader: 'postcss-loader',
+								   options: {
+									   sourceMap: env.production !== true
+								   }
+							   }
+						   ]
+					   }
+				   ]
+			   }
+			]
+		}
+	}
 };
